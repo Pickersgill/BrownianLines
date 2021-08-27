cc66cc52e81783a914a167c3ac71686ac5623266
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2021-08-27 18:17:28 +0100

    working line drawing!

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 import classes
 import style
+import random
 from PIL import Image, ImageDraw
 
 Point = classes.Point
@@ -19,10 +20,25 @@ class DrawField:
     def get_field_img(self):
         return self.img
 
+    def draw_line_with_outline(self, points, colour, width):
+        self.draw.line(points, style.HERO_OUTLINE_COL, width + (style.HERO_OUTLINE_WIDTH * 2))
+        for point in points:
+            self.draw.circle(point[0], point[1], (style.HERO_LINE_W // 2) + style.HERO_OUTLINE_WIDTH, \
+            fill=style.HERO_OUTLINE_COL)
+
+        self.draw.line(points, colour, width)
+        for point in points:
+            self.draw.circle(point[0], point[1], (style.HERO_LINE_W // 2), fill=colour)
+
     def render(self):
         for h in self.field.heroes:
             for trail in h.trails:
-                self.draw.line(trail, style.HERO_COL, style.HERO_LINE_W)
+                if style.RANDOM_COL:
+                    temp_col = random.choice(style.HERO_COLS)
+                else:
+                    temp_col = style.HERO_COL
+
+                self.draw_line_with_outline(trail, temp_col, style.HERO_LINE_W)
 
 field = Field(style.PARTICLE_NUM, style.HERO_NUM)
 draw = DrawField(field)
